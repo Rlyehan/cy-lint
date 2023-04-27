54274a1908bd0ccced723c8b6f6e15a2e8df0d5b
Maximilian Huber <max_hub@gmx.net>
2023-04-27 11:26:31 +0200

    updated CLI interface

--- src/main.ts
+++ src/main.ts
@@ -1,97 +1,98 @@
 import * as fs from "fs";
 import * as path from "path";
-import * as ts from "typescript"
+import * as ts from "typescript";
+import { Argv } from "yargs";
+const yargs = require("yargs");
+const { hideBin } = require("yargs/helpers");
 import { ruleRegistry } from "./rulesRegistry";
 import { Rule } from "./rules";
 
-const args = process.argv.slice(2);
-
-if (args.length === 0) {
-    console.log("Please provide a path to your config file");
-} else {
-    const configPath = args[0];
-    console.log(`Analyzing your Cypress tests using the configuration file: ${configPath}`);
-
-    const ruleConfig: Rule[] = JSON.parse(fs.readFileSync(configPath, "utf8"));
-
-    const RuleFunctions = ruleConfig
-        .filter((rule: Rule) => rule.enabled)
-        .map((rule: Rule) => ruleRegistry[rule.id]);
-
-    console.log('Enabled rules:', RuleFunctions);
-
-    const testDirectory = args[1];
-    const testFiles = parseTestFiles(testDirectory);
-
-    console.log('Test files to analyze:', testFiles);
-
-    const violations: any[] = [];
-
-    testFiles.forEach((testFile) => {
-        console.log(`Analyzing test file: ${testFile}`);
-        violations.push(...analyzeTestFile(testFile, RuleFunctions));
-    });
-
-    if (violations.length === 0) {
-        console.log("No violations found");
-    } else {
-        console.log("Found violations:");
-        violations.forEach((violation) => {
-            console.log(violation);
-        });
-        const reportPath = "violations_report.txt";
-        generateReport(violations, reportPath);
-        console.log(`Report saved to ${reportPath}`);
+yargs(hideBin(process.argv))
+.command(
+  "$0 <config> [testDirectory]",
+  "Analyze Cypress tests using the provided configuration file",
+  (yargs: Argv) => {
+    yargs
+      .positional("config", {
+        describe: "Path to the configuration file",
+        type: "string",
+        demandOption: true,
+      })
+      .positional("testDirectory", {
+        describe: "Path to the directory containing the test files",
+        type: "string",
+        default: "./",
+      });
+  },
+  (argv: { config: string; testDirectory: string }) => {
+      main(argv.config as string, argv.testDirectory as string);
     }
+  )
+  .demandCommand(1, "You must provide a valid command")
+  .help()
+  .strict()
+  .parse();
+
+function main(configPath: string, testDirectory: string) {
+  console.log(`Analyzing your Cypress tests using the configuration file: ${configPath}`);
+
+  const ruleConfig: Rule[] = JSON.parse(fs.readFileSync(configPath, "utf8"));
+
+  const RuleFunctions = ruleConfig
+    .filter((rule: Rule) => rule.enabled)
+    .map((rule: Rule) => ruleRegistry[rule.id]);
+
+  const testFiles = parseTestFiles(testDirectory);
+
+  const violations: any[] = [];
+
+  testFiles.forEach((testFile) => {
+    violations.push(...analyzeTestFile(testFile, RuleFunctions));
+  });
+
+  if (violations.length === 0) {
+    console.log("No violations found");
+  } else {
+    console.log("Found violations:");
+    violations.forEach((violation) => {
+      console.log(violation);
+    });
+  }
 }
 
 function parseTestFiles(directory: string): string[] {
-    const files = fs.readdirSync(directory);
-    const testFiles: string[] = [];
+  const files = fs.readdirSync(directory);
+  const testFiles: string[] = [];
 
-    files.forEach((file) => {
-        const filePath = path.join(directory, file);
-        const stat = fs.statSync(filePath);
+  files.forEach((file) => {
+    const filePath = path.join(directory, file);
+    const stat = fs.statSync(filePath);
 
-        if (stat.isDirectory()) {
-            testFiles.push(...parseTestFiles(filePath));
-        } else if (file.endsWith(".cy.ts")) {
-            testFiles.push(filePath);
-        }
-    });
+    if (stat.isDirectory()) {
+      testFiles.push(...parseTestFiles(filePath));
+    } else if (file.endsWith(".cy.ts")) {
+      testFiles.push(filePath);
+    }
+  });
 
-    return testFiles;
+  return testFiles;
 }
 
 function analyzeTestFile(filePath: string, RuleFunctions: any[]): any[] {
-    const sourceCode = fs.readFileSync(filePath, "utf8");
-    const sourceFile = ts.createSourceFile(filePath, sourceCode, ts.ScriptTarget.Latest, true);
+  const sourceCode = fs.readFileSync(filePath, "utf8");
+  const sourceFile = ts.createSourceFile(filePath, sourceCode, ts.ScriptTarget.Latest, true);
 
-    const violations: any[] = [];
+  const violations: any[] = [];
 
-    function visit(node: ts.Node) {
-        RuleFunctions.forEach((RuleFunction) => {
-            violations.push(...RuleFunction(node));
-        });
-
-        ts.forEachChild(node, visit);
-    }
-
-    visit(sourceFile);
-
-    return violations;
-}
-
-function generateReport(violations: any[], outputPath: string): void {
-    const reportLines: string[] = [];
-  
-    violations.forEach((violation) => {
-      reportLines.push(
-        `${violation.filepath}:${violation.line} - ${violation.description}`
-      );
+  function visit(node: ts.Node) {
+    RuleFunctions.forEach((RuleFunction) => {
+      violations.push(...RuleFunction(node));
     });
-  
-    const reportContent = reportLines.join("\n");
-  
-    fs.writeFileSync(outputPath, reportContent);
+
+    ts.forEachChild(node, visit);
   }
+
+  visit(sourceFile);
+
+  return violations;
+}
