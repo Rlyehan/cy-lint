5263773badc5ca06adb6b5fd944fddfe72f947f9
Maximilian Huber <max_hub@gmx.net>
2023-05-08 07:44:30 +0200

    ran prettier

--- sample_tests/example.cy.ts
+++ sample_tests/example.cy.ts
@@ -3,7 +3,7 @@
 describe("Sample test suite", () => {
   it("Sample test case", () => {
     cy.visit("/");
-    cy.wait(1000); // This line should trigger a violation for the "no-hardcoded-wait" rule
-    cy.get("#element"); // This line should trigger a violation for the "use-data-attributes" rule
+    cy.wait(1000); 
+    cy.get("#element"); 
   });
 });
